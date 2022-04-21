d104b71bde52be9d32522d46936645b3e87b6a9d
harshakakimani-deloitte <harshakakimani-deloitte@users.noreply.github.com>
2022-04-21 19:42:41 +0530

    Subject Experte Home Page

--- Resume_Hunter/src/test/java/tests/subjectExperteHomePageTest.java
+++ Resume_Hunter/src/test/java/tests/subjectExperteHomePageTest.java
@@ -1,4 +1,6 @@
 package tests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import pages.*;
 
 import org.openqa.selenium.WebDriver;
@@ -8,12 +10,17 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class subjectExperteHomePageTest {
-    @Test
-    private static void test1() throws InterruptedException, IOException {
-        WebDriver driver;
+    static WebDriver driver;
+    @BeforeTest
+    private static void initialSetup(){
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaharshavardhan\\Downloads\\chromedriver.exe");
         driver=new ChromeDriver();
         driver.get("https://resume-filter-frontend-urtjok3rza-wl.a.run.app/login");
+
+    }
+    @Test
+    private static void subjectExperteHomePage() throws InterruptedException, IOException {
         subjectExperteHomePage subHome=new subjectExperteHomePage(driver);
         subHome.login();
         subHome.countingNoOfRecuriment();
