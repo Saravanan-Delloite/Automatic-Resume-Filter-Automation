6ef42fa04cd8e7c4401ae8df23c013bd923701b8
Manoj <javvkumar@deloitte.com>
2022-04-22 21:22:43 +0530

    Added test cases for HR signup and SubjectExpert signup

--- ARM/src/test/java/TestLibrary/MainTest.java
+++ ARM/src/test/java/TestLibrary/MainTest.java
@@ -1,18 +1,25 @@
 package TestLibrary;
 
 import PageLibrary.DriverPage;
-import PageLibrary.SignupPage;
+//import PageLibrary.SignupOperation;
+import PageLibrary.SignupOperation;
+import PageLibrary.SignupPageDisplayAndEnable;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class MainTest extends DriverPage {
     DriverPage dr = new DriverPage();
-    SignupPage signup =new SignupPage(driver);
-//@Parameters("browserName")
+    SignupPageDisplayAndEnable signup =new SignupPageDisplayAndEnable(driver);
+    SignupOperation operation= new SignupOperation(driver);
+
+
+
+    //@Parameters("browserName")
 @BeforeTest
 @Deprecated
     public void openBrowser()
@@ -56,19 +63,27 @@ public class MainTest extends DriverPage {
     boolean email = signup.emailTextFieldEnable();
     Assert.assertEquals(email,true);
 
-    boolean userName = signup.emailTextFieldEnable();
+    boolean userName = signup.userNameTextFieldEnable();
     Assert.assertEquals(userName,true);
 
-    boolean password = signup.emailTextFieldEnable();
+    boolean password = signup.passwordTextFieldEnable();
     Assert.assertEquals(password,true);
 
-    boolean confirmPassword = signup.emailTextFieldEnable();
+    boolean confirmPassword = signup.confirmPasswordTextFieldEnable();
     Assert.assertEquals(confirmPassword,true);
 
     boolean role = signup.roleTextFieldEnable();
     Assert.assertEquals(role,true);
 
 }
+
+@Test(priority = 4)
+public void Signup() throws InterruptedException {
+    operation.userSignup();
+//    operation.HR();
+    operation.subjectExpert();
+}
+
 
 @AfterTest
     public void closeBrowser()
