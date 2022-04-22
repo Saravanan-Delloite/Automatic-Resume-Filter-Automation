7ed6c5833909a57f7eaa99a39870d15a07d765a1
karthikck <karthikck@deloitte.com>
2022-04-22 12:38:51 +0530

    Expert Add Quiz Page Initial Commit

---
+++ AutomaticResumeFilter/src/main/java/Page/AddQuizPage.java
@@ -0,0 +1,58 @@
+package Page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AddQuizPage {
+    WebDriver driver;
+
+    public AddQuizPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    By username_box = By.xpath("//*[@id='emailAddress']");
+    By password_box = By.xpath("//*[@id='password']");
+    By loginBtn = By.xpath("//button[text()=' Login ']");
+    By addQuizBtn =By.xpath("/html/body/app-root/app-expert-dashboard/div[2]/div/app-expert-cand-cards[11]/div/div/div[3]/button");
+
+    By homeBtn = By.xpath("//a[text()='Home']");
+    By quizLink = By.xpath("//*[@id='quizlink']");
+    By date = By.xpath("//*[@id='date']");
+    By timeLimit = By.xpath("//input[@formcontrolname='due_time']");
+    By numberOfQue = By.xpath("//input[@formcontrolname='questions']");
+    By reset = By.xpath("//button[@type='reset']");
+    By submit = By.xpath("//button[text()=' Submit Quiz ']");
+
+    public void clickHomeBtn(){
+        driver.findElement(homeBtn).click();
+    }
+
+    public void login(String username, String password) throws InterruptedException {
+        driver.findElement(username_box).sendKeys(username);
+        driver.findElement(password_box).sendKeys(password);
+        driver.findElement(loginBtn).click();
+        Thread.sleep(5000);
+        driver.get("https://resume-filter-frontend-urtjok3rza-wl.a.run.app/expert/add-quiz/183");
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()=' Add Quiz']"))).click();
+//        driver.findElement(By.xpath("//button[text()=' Add Quiz']")).click();
+//        List<WebElement> buttons = driver.findElements(By.xpath("//button[text()=' Add Quiz']"));
+//        for (int i=0; i<buttons.size();i++){
+//            WebElement button = buttons.get(i);
+//            if (button.isEnabled()){
+//                System.out.println(button);
+//                button.click();
+//                break;
+//            }
+//        }
+//
+    }
+
+    public void addQuizDetails (String link, String dueDate, String time, String questions){
+        driver.findElement(quizLink).sendKeys(link);
+        driver.findElement(date).sendKeys(dueDate);
+        driver.findElement(timeLimit).sendKeys(time);
+        driver.findElement(numberOfQue).sendKeys(questions);
+        driver.findElement(submit).click();
+    }
+}
---
+++ AutomaticResumeFilter/src/test/java/TestPage/AddQuizPageTest.java
@@ -0,0 +1,37 @@
+package TestPage;
+
+import Page.AddQuizPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class AddQuizPageTest {
+    ReadDataFromExcel readDataFromExcel=new ReadDataFromExcel();
+    AddQuizPage addQuizPage;
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialSetup(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\karthikck\\Downloads\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://resume-filter-frontend-urtjok3rza-wl.a.run.app/login");
+    }
+
+    @Test
+    public void addQuiz() throws InterruptedException, IOException {
+        addQuizPage = new AddQuizPage(driver);
+        addQuizPage.login("expert","expert@123");
+        String link = readDataFromExcel.sendData(1, 0);
+        String date = (readDataFromExcel.sendData(1, 1));
+        String time= readDataFromExcel.sendData(1, 2);
+        String questions = readDataFromExcel.sendData(1, 3);
+        addQuizPage.addQuizDetails(link, date, time, questions);
+        driver.close();
+    }
+}
