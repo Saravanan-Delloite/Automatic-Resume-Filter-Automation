430acdcd32b45c21531c7e1585f76364a5e6ec26
Manoj <javvkumar@deloitte.com>
2022-04-22 21:18:44 +0530

    Reading Data from the Excel sheet

---
+++ ARM/src/main/java/PageLibrary/ReadingExcel.java
@@ -0,0 +1,78 @@
+package PageLibrary;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadingExcel {
+    private String name,email,username,skills;
+    private int password,confirmPassword;
+    public void readData()  {
+        try {
+
+            String excelFilePath = "src/main/resources/ARF Signup.xlsx";
+            FileInputStream inputStream = new FileInputStream(excelFilePath);
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            int rows = sheet.getLastRowNum();
+            int cols = sheet.getRow(1).getLastCellNum();
+
+            for (int i = 1; i <= rows; i++) {
+                XSSFRow row = sheet.getRow(i);
+                for (int j = 0; j < cols; j++) {
+                    XSSFCell cell = row.getCell(j);
+                    if (j == 0) {
+                        name = cell.getStringCellValue();
+                    }
+                    if (j == 1) {
+                        email = cell.getStringCellValue();
+                    }
+                    if (j == 2) {
+                        username = cell.getStringCellValue();
+                    }
+                    if (j == 3) {
+                        password = (int) cell.getNumericCellValue();
+                    }
+                    if (j == 4) {
+                        confirmPassword = (int) cell.getNumericCellValue();
+                    }
+                    if (j == 5) {
+                        skills =  cell.getStringCellValue();
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public String getUsername()
+    {
+        return  username;
+    }
+    public int getPassword()
+    {
+        return password;
+    }
+    public int getConfirmPassword()
+    {
+        return confirmPassword;
+    }
+    public String getSkills()
+    {
+        return skills;
+    }
+}
