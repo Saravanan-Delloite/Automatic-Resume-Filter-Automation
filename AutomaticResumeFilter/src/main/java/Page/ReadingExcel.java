954fce04caedc503e50ae2f6ccacc5bd8fa3671b
Manoj <javvkumar@deloitte.com>
2022-04-25 16:07:24 +0530

    Modified data with different file

--- AutomaticResumeFilter/src/main/java/Page/ReadingExcel.java
+++ AutomaticResumeFilter/src/main/java/Page/ReadingExcel.java
@@ -1,4 +1,4 @@
-package Page;
+package PageLibrary;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -6,72 +6,24 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ReadingExcel {
-    private String name,email,username,skills;
-    private int password,confirmPassword;
-    public void readData()  {
+    //    private String name, email, username, skills;
+//    private int password, confirmPassword;
+    static XSSFWorkbook wbook;
+    static XSSFSheet sheet;
+    public static void read()  {
         try {
-
-            String excelFilePath = "C:\\Users\\yashmittal2\\Desktop\\Automatic-Resume-Filter-Automation\\AutomaticResumeFilter\\src\\resources\\ARF Signup.xlsx";
-            FileInputStream inputStream = new FileInputStream(excelFilePath);
-            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            int rows = sheet.getLastRowNum();
-            int cols = sheet.getRow(1).getLastCellNum();
-
-            for (int i = 1; i <= rows; i++) {
-                XSSFRow row = sheet.getRow(i);
-                for (int j = 0; j < cols; j++) {
-                    XSSFCell cell = row.getCell(j);
-                    if (j == 0) {
-                        name = cell.getStringCellValue();
-                    }
-                    if (j == 1) {
-                        email = cell.getStringCellValue();
-                    }
-                    if (j == 2) {
-                        username = cell.getStringCellValue();
-                    }
-                    if (j == 3) {
-                        password = (int) cell.getNumericCellValue();
-                    }
-                    if (j == 4) {
-                        confirmPassword = (int) cell.getNumericCellValue();
-                    }
-                    if (j == 5) {
-                        skills =  cell.getStringCellValue();
-                    }
-                }
-            }
+            FileInputStream inputStream = new FileInputStream("src/main/resources/ARF Signup.xlsx");
+            wbook = new XSSFWorkbook(inputStream);
+            sheet = wbook.getSheetAt(0);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (Exception e)
-        {
-            System.out.println(e);
-        }
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public String getEmail()
-    {
-        return email;
-    }
-    public String getUsername()
-    {
-        return  username;
-    }
-    public int getPassword()
-    {
-        return password;
-    }
-    public int getConfirmPassword()
-    {
-        return confirmPassword;
-    }
-    public String getSkills()
-    {
-        return skills;
+
     }
 }
