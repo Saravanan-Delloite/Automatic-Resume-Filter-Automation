f5014aaf41e23256973f04cabee6077e855aee1f
Manoj <javvkumar@deloitte.com>
2022-04-28 20:46:10 +0530

    Added Logs.

--- AutomaticResumeFilter/src/test/java/TestPage/SignUpTest.java
+++ AutomaticResumeFilter/src/test/java/TestPage/SignUpTest.java
@@ -4,6 +4,7 @@ import Page.*;
 //import PageLibrary.SignupOperation;
 import Utilities.Environment;
 import Utilities.ListenerForExtentReport;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -14,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 
 public class SignUpTest {
-
+    Logger logger = Logger.getLogger(SignUpTest.class);
     SignupPageDisplayAndEnable signup;
     SignupOperation operation;
     @BeforeSuite
@@ -22,7 +23,7 @@ public class SignUpTest {
         setDriver();
         getDriver().get(Environment.URL);
         getDriver().manage().window().maximize();
-
+        logger.info("Opening Browser and Maximizing It");
         signup =new SignupPageDisplayAndEnable(getDriver());
         operation= new SignupOperation(getDriver());
     }
@@ -41,6 +42,7 @@ public class SignUpTest {
     public void signupPage()
     {
         signup.clickRegister();
+        logger.info("Clicking the Signup Link");
         String text = signup.gettext();
         Assert.assertEquals(text,"Sign Up");
     }
@@ -49,42 +51,54 @@ public class SignUpTest {
     {
         boolean name = signup.nameTextFieldDisplay();
         Assert.assertEquals(name,true);
+        logger.info("Checking Name Field is Displayed");
 
         boolean email = signup.emailTextFieldDisplay();
         Assert.assertEquals(email,true);
+        logger.info("Checking email Field is Displayed");
 
         boolean userName = signup.userNameTextFieldDisplay();
         Assert.assertEquals(userName,true);
+        logger.info("Checking userName Field is Displayed");
 
         boolean password = signup.passwordTextFieldDisplay();
         Assert.assertEquals(password,true);
+        logger.info("Checking password Field is Displayed");
 
-        boolean cinformPassword = signup.confirmPasswordTextFieldDisplay();
-        Assert.assertEquals(cinformPassword,true);
+        boolean confirmPassword = signup.confirmPasswordTextFieldDisplay();
+        Assert.assertEquals(confirmPassword,true);
+        logger.info("Checking confirm password Field is Displayed");
 
         boolean role = signup.roleTextFieldDisplay();
         Assert.assertEquals(role,true);
+        logger.info("Checking role Field is Displayed");
     }
     @Test(priority = 3)
     public void textFieldEnable()
     {
         boolean name = signup.nameTextFieldEnable();
         Assert.assertEquals(name,true);
+        logger.info("Checking Name Field is Enable");
 
         boolean email = signup.emailTextFieldEnable();
         Assert.assertEquals(email,true);
+        logger.info("Checking email Field is Enable");
 
         boolean userName = signup.userNameTextFieldEnable();
         Assert.assertEquals(userName,true);
+        logger.info("Checking userName Field is Enable");
 
         boolean password = signup.passwordTextFieldEnable();
         Assert.assertEquals(password,true);
+        logger.info("Checking password Field is Enable");
 
         boolean confirmPassword = signup.confirmPasswordTextFieldEnable();
         Assert.assertEquals(confirmPassword,true);
+        logger.info("Checking confirm password Field is Enable");
 
         boolean role = signup.roleTextFieldEnable();
         Assert.assertEquals(role,true);
+        logger.info("Checking role Field is Enable");
 
     }
 
@@ -92,12 +106,14 @@ public class SignUpTest {
     public void SignupHR() throws InterruptedException {
         operation.userSignupHR();
         operation.HR();
+        logger.info("User signed in as HR");
         Thread.sleep(3000);
     }
     @Test(priority = 5)
     public void SignupSubExp() throws InterruptedException {
         operation.userSignupSubjectExpert();
         operation.subjectExpert();
+        logger.info("User signed in as Subject Expert");
         Thread.sleep(3000);
     }
 
